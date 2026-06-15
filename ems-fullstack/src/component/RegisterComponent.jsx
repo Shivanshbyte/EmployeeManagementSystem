cefa45113a4d921d3d77ef28714ef0c6f8637da8
Shivanshbyte <shivanshpathak90@gmail.com>
2026-06-16 03:34:19 +0530

    Syntax fix

--- ems-fullstack/src/component/RegisterComponent.jsx
+++ ems-fullstack/src/component/RegisterComponent.jsx
@@ -17,7 +17,7 @@ function RegisterComponent() {
         try {
 
             await axios.post(
-                "`${import.meta.env.VITE_API_URL}/api/auth/register`",
+                `${import.meta.env.VITE_API_URL}/api/auth/register`,
                 {
                     username,
                     password,
